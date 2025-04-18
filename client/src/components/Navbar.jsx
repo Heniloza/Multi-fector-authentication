45dfa87c4d538ef0ec04a5c985adaef3bd29fff9
Heniloza <ozahenil73@gmail.com>
2025-04-18 22:09:42 +0530

    navbar and home page created successfully

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaArrowRightLong } from "react-icons/fa6";
+
+function Navbar({ isLoggedIn, username, handleLogout }) {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const firstLetter = username ? username.charAt(0).toUpperCase() : "";
+
+  const toggleDropDown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  const closeDropdown = () => {
+    setDropdownOpen(false);
+  };
+
+  return (
+    <nav className="w-full h-20 flex justify-between items-center px-8">
+      <div>
+        <Link to="/">DevAuth</Link>
+      </div>
+      <div>
+        {!isLoggedIn ? (
+          <div className="p-2 border-2 rounded-2xl px-6 flex justify-center items-center gap-2">
+            <Link to="/signin">Login</Link>
+            <div className="mt-1">
+              <FaArrowRightLong />
+            </div>
+          </div>
+        ) : (
+          <div className="relative  inline-block">
+            <div
+              onClick={toggleDropDown}
+              className="w-10 h-10 rounded-full flex justify-center items-center bg-black text-white cursor-pointer"
+            >
+              {firstLetter}
+            </div>
+            {dropdownOpen && (
+              <div className="absolute  right-4 mt-2 bg-white text-black p-2 rounded shadow z-10">
+                <button
+                  onClick={() => {
+                    handleLogout();
+                    closeDropdown();
+                  }}
+                  className="block px-4 py-2"
+                >
+                  Logout
+                </button>
+                <Link
+                  onClick={closeDropdown}
+                  to="/profile"
+                  className="block px-4 py-2"
+                >
+                  Profile
+                </Link>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 function Home() {
-  return <div>Home</div>;
+  const isLoggedIn = true;
+  const username = "Henil Shrimali";
+  const handleLogout = () => {};
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar
+        isLoggedIn={isLoggedIn}
+        username={username}
+        handleLogout={handleLogout}
+      />
+      <div className="flex-1 flex flex-col gap-8 justify-center items-center">
+        <div className="flex flex-col gap-2 justify-center items-center">
+          <h1 className="font-extrabold text-3xl tracking-wider">
+            {isLoggedIn ? `Hello, ${username}` : "Hello,Developer"}
+          </h1>
+          <h1 className="font-extrabold text-2xl tracking-widert">
+            Welcome to our App
+          </h1>
+        </div>
+        <div>
+          <button className="px-6 py-2 bg-white text-black font-bold rounded-3xl border-2 hover:bg-pink-500 hover:text-white transition duration-500 border-black cursor-pointer">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
